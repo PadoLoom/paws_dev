a0c8a31fc6664576ce302958cfcdfb0bfdbbb3fd
raymond_cty <cty0613@hanmail.net>
2024-05-31 15:51:03 +0900

    Fix: Notificatoin Permissions Works

--- android/app/src/main/kotlin/com/example/paws_dev/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/paws_dev/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.paws_dev
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
-import co.euphony.rx.AcousticSensor
 import co.euphony.rx.EuRxManager
 import co.euphony.util.EuOption
 import io.flutter.embedding.android.FlutterActivity
@@ -57,6 +63,46 @@ class MainActivity: FlutterActivity() {
     }
 
 
+    /* TODO : Request Notification Permissions */
+    private fun showNotification(title: String, content: String) {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationId = 1
+        val channelId = "flutter_notification_channel"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Notification Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Channel description"
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,
+            0,
+            intent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_IMMUTABLE
+            } else {
+                0
+            })
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        notificationManager.notify(notificationId, builder.build())
+    }
+
     private fun requestAudioPermissions() {
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
     }
@@ -74,9 +120,11 @@ class MainActivity: FlutterActivity() {
             Log.v(TAG, "startReceiver")
 
             mRxManager.setOnWaveKeyUp(19000){
-                Log.d(TAG, "_________[Key Up Received]__________")
+                Log.d(TAG, "----------------[Key Up Received]----------------")
+                showNotification("Warning", "COLLISION HAZARD!")
+
                 mRxManager.finish()
-//                Toast(this, "KeyUp Received", Toast.LENGTH_SHORT).show()
+                Log.d(TAG, "----------------[setOnWaveKeyUp Restart]----------------")
                 mRxManager.listen()
             }
             
@@ -90,7 +138,6 @@ class MainActivity: FlutterActivity() {
     }
     private fun stopReceiver() {
         mRxManager.finish()
-//        receiveState = false
         Toast.makeText(this, "Service Terminated", Toast.LENGTH_SHORT).show()
         Log.v(TAG,"stopReceiver")
     }
